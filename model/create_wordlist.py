66780f8e587b0a81fcc1c6978be0e4b4c3a0e0ef
Sumedha <pramods@umich.edu>
2014-11-25 18:08:27 -0500

    Separated words into categories

--- model/create_wordlist.py
+++ model/create_wordlist.py
@@ -7,7 +7,8 @@ wordArr = word_file.readlines()
 word_file.close()
 
 wlist.write("var wordList = [")
-gram = "var grammar = {numStates: 1, start: 0, end: 0, transitions: ["
+gram_footer = "]};\n\n"
+gram = ""
 words = ""
 
 for word in wordArr:
@@ -15,7 +16,27 @@ for word in wordArr:
   splitWord = word.split(' ')
   print(splitWord)
   words += "[\"" + splitWord[0] + "\", \"" + ' '.join(splitWord[1:]) + "\"], "
-  gram += "{from: 0, to: 0, word: \"" + splitWord[0] + "\"}, "
 
-wlist.write(words[:-2] + "];\n" + gram[:-2] + "]};")
+wlist.write(words[:-2] + "];\n\n")
+
+gram_lists = ["appetizer", "beverages", "breads", "curries", "desserts", "none", "number", "proteins", "rice", "sides", "south", "vegetables", "vegetarian"]
+
+for category in gram_lists:
+  word_file = open(category, "r") 
+  wordArr = word_file.readlines()
+  word_file.close()
+
+  header = "var gram_" + category + " = {\n\tnumStates: 1, start: 0, end: 0, transitions: [\n\t"
+  gram += header
+
+  for word in wordArr:
+    word = word.strip('\n')
+    splitWord = word.split(' ')
+    print(splitWord)
+    words += "[\"" + splitWord[0] + "\", \"" + ' '.join(splitWord[1:]) + "\"], "
+    gram += "{from: 0, to: 0, word: \"" + splitWord[0] + "\"},\n\t"
+
+  gram = gram[:-3] + "\n" + gram_footer
+
+wlist.write(gram)
 wlist.close()
